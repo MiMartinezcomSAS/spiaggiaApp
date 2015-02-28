9649f182ca85bf0f897f85df6181c7cca895578d
juan2ramos <juan2ramos@gmail.com>
2015-02-28 10:45:00 -0500

    + Home background and logo

--- www/js/index.js
+++ www/js/index.js
@@ -1,25 +1,10 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+var wrapper = document.getElementById("wrapper"),
+    xhReq = new XMLHttpRequest();
 var app = {
-    // Application Constructor
+
     initialize: function() {
         this.bindEvents();
+        loadHome();
     },
     // Bind Event Listeners
     //
@@ -37,6 +22,8 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
+        document.addEventListener("backbutton", onBackKeyDown, false);
+        document.addEventListener('orientationchange',  rotacion,  false);
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -47,3 +34,15 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+function loadHome(){
+    xhReq.open("GET", "home.html", false);
+    xhReq.send(null);
+    document.getElementById("content-page").innerHTML = xhReq.responseText
+    if ($('.sliderHome').length > 0) {
+        $('.sliderHome').flexslider({
+            animation: "fade",
+            controlNav: false,
+            directionNav: false
+        });
+    }
+}
